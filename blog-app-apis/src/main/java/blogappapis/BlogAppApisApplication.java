19635e1801bfc4477d6d86c9c2fdc4f0c36fdae9
AbhijeetSingh <abhi9anc@gmail.com>
2022-11-18 15:03:26 +0530

    first commit comment

--- blog-app-apis/src/main/java/blogappapis/BlogAppApisApplication.java
+++ blog-app-apis/src/main/java/blogappapis/BlogAppApisApplication.java
@@ -10,7 +10,7 @@ public class BlogAppApisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BlogAppApisApplication.class, args);
-		System.out.println("Started !!");
+//		System.out.println("Started !!");
 	}
 	
 	@Bean
